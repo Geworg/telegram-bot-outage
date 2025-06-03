3914d2011cc9cf31486c2ba5d9cca483bf0887db
Gevorg <gevorgt561@gmail.com>
2025-06-03 19:50:39 +0400

    correct

--- smart_bot.py
+++ smart_bot.py
@@ -1,6 +1,3 @@
-import telegram
-print("PTB version:", telegram.__version__)
-
 import os
 import json
 import re
@@ -17,8 +14,7 @@ import urllib.parse
 import hashlib
 from dotenv import load_dotenv
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
-from telegram.ext import Application, ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, MessageHandler, filters, BasePersistence, JobQueue # PicklePersistence
-from telegram.ext.persistence import JSONPersistence
+from telegram.ext import Application, ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, MessageHandler, filters, BasePersistence, JobQueue, JSONPersistence # PicklePersistence
 from telegram.constants import ParseMode
 from telegram.error import Forbidden, RetryAfter, TimedOut, NetworkError
 from logger import log_info, log_error, log_warning
